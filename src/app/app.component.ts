41b28613578e97bea254b281269786fe99849e66
CHOQUETTEC <catherine.choquette@dfo-mpo.gc.ca>
2022-12-19 15:30:55 -0500

    Assignment 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`
+    .overFive{
+        color : white;
+    }
+  `]
 })
 export class AppComponent {
-  title = 'basics-assignment-3-start';
+  showDetails=false;
+  buttonClicks=[];
+  // count='one';
+
+  onDisplayDetails() {
+    this.showDetails = !this.showDetails;
+    this.buttonClicks.push(this.buttonClicks.length + 1);
+    // this.count='two';
+
+  }
+
+  getColor() {
+    return this.buttonClicks.length >= 5 ? 'blue':'';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { ParagraphComponent } from './paragraph/paragraph.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParagraphComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
